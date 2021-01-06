1c08e0d7b99b78a35d476ed69aebc514b21d10e0
Eeva-Jonna Panula <eeva-jonna.panula@futurice.com>
2021-01-06 15:49:54 +0200

    Add apollo client setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,26 @@
+import { gql, useQuery } from "@apollo/client"
 import { NextPage } from "next"
 import Head from "next/head"
 
+const GET_USER = gql`
+  query {
+    user(userId: 2) {
+      id
+      email
+      username
+    }
+  }
+`
+
 const Home: NextPage = () => {
+  const { data } = useQuery(GET_USER)
   return (
     <>
       <Head>
         <title>Template</title>
       </Head>
 
-      <main></main>
+      <main>{data && <section>{data.user.username}</section>}</main>
     </>
   )
 }
