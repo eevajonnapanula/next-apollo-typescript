5178be683c9a71bf8e42853e8c5f878f70ffd169
Eeva-Jonna Panula <eeva-jonna.panula@futurice.com>
2021-01-06 15:45:00 +0200

    Add ts, prettier and eslint

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import { NextPage } from "next"
+import Head from "next/head"
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Template</title>
+      </Head>
+
+      <main></main>
+    </>
+  )
+}
+
+export default Home
